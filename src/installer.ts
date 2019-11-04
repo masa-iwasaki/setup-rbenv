fcac2e355e002e14c1e9dcbd3fba9eb00be36aa7
Masatoshi Iwasaki <mstshiwasaki@gmail.com>
2019-11-04 22:20:51 +0900

    export doesn't work on child process

--- src/installer.ts
+++ src/installer.ts
@@ -2,7 +2,8 @@ import * as exec from '@actions/exec';
 
 export async function install_rbenv() {
   await exec.exec('git', ['clone', 'https://github.com/rbenv/rbenv.git', '~/.rbenv']);
-  await exec.exec('export', ['PATH="$HOME/.rbenv/bin:$PATH"']);
+  // TODO: Add README that PATH should be set by users
+  // await exec.exec('export', ['PATH="$HOME/.rbenv/bin:$PATH"']);
 }
 
 export async function install_ruby_build() {
