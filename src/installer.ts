fe45649293b01905b138d524ce0460866e9c600e
Masatoshi Iwasaki <mstshiwasaki@gmail.com>
2019-12-10 16:40:00 +0900

    Remove comments for implemented features

--- src/installer.ts
+++ src/installer.ts
@@ -2,8 +2,6 @@ import * as exec from '@actions/exec';
 
 export async function intallRbenv(options: RbenvOptions) {
   await exec.exec('sudo', ['git', 'clone', 'https://github.com/rbenv/rbenv.git', options.rbenvRoot]);
-  // TODO: Add README that PATH should be set by users
-  // await exec.exec('export', ['PATH="$HOME/.rbenv/bin:$PATH"']);
 }
 
 export interface RbenvOptions {
